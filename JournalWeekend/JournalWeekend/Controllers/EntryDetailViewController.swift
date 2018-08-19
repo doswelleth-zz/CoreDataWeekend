903a8b66bd0290062b5547b1f37732e053374655
Lambda-School-Loaner-11 <lambda-school-loaner-11@Lambda-School-Loaner-11s-MacBook-Pro.local>
2018-08-19 13:02:22 -0700

    set up storyboards

---
+++ JournalWeekend/JournalWeekend/Controllers/EntryDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EntryDetailViewController.swift
+//  JournalWeekend
+//
+//  Created by Lambda-School-Loaner-11 on 8/19/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ JournalWeekend/JournalWeekend/Views/EntryTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  EntryTableViewCell.swift
+//  JournalWeekend
+//
+//  Created by Lambda-School-Loaner-11 on 8/19/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var bodyText: UILabel!
+    
+    @IBOutlet weak var timestamp: UILabel!
+    
+    
+    
+}
